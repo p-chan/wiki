47e87365e104322fc1718c5daadc6a1eea12e3a7
P-Chan <hello@0x50.io>
2020-03-27 23:29:04 +0900

    feat: ress を追加

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import 'ress'
 import { IndexComponent, AboutComponent } from './pages'
 import { firebase } from './utilities'
 
