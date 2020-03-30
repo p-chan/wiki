cb9338858ae1b7d8f4d76186153b6baeab5eacf8
P-Chan <hello@0x50.io>
2020-03-31 05:55:57 +0900

    feat: 改行を br タグに変換するプラグインを追加

--- src/utilities/markdown.ts
+++ src/utilities/markdown.ts
@@ -4,8 +4,11 @@ import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 // @ts-ignore
 import rehypeStringify from 'rehype-stringify'
+// @ts-ignore
+import remarkBreaks from 'remark-breaks'
 
 export const markdown = unified()
   .use(remarkParse)
+  .use(remarkBreaks)
   .use(remarkRehype)
   .use(rehypeStringify)
