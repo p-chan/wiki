3146f771c0b3071b2ac83237540c81f22a74d90a
P-Chan <hello@0x50.io>
2020-05-21 07:54:48 +0900

    feat: ヘッダーのデザインを変更

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import shortid from 'shortid'
 import classnames from 'classnames'
+import headerIconImagePath from './header-icon.png'
 
 import styles from './header.css'
 
@@ -18,8 +19,19 @@ export const HeaderComponent = (props: Props) => {
 
   return (
     <div className={classnames(styles.wrapper, props.wrapperClassName)}>
-      <Link to={'/'}>Wiki</Link>
-      <div onClick={onClickNewButton}>新しい記事を作る</div>
+      <Link to={'/'} className={styles.logo}>
+        <img
+          src={headerIconImagePath}
+          className={styles.logoIcon}
+          width={32}
+          height={32}
+          alt="P-Chan"
+        />
+        <span className={styles.logoText}>Wiki</span>
+      </Link>
+      <button onClick={onClickNewButton} className={styles.button}>
+        新しい記事を作る
+      </button>
     </div>
   )
 }
--- src/components/header.css.d.ts
+++ src/components/header.css.d.ts
@@ -1,5 +1,9 @@
 declare const styles: {
   readonly "wrapper": string;
+  readonly "logo": string;
+  readonly "logoIcon": string;
+  readonly "logoText": string;
+  readonly "button": string;
 };
 export = styles;
 
